e07f7b8e310a33d2ebe1ff97a65e8a3beb195e27
Pantoja <jvpantojamaciel@gmail.com>
2025-06-06 12:06:02 -0300

    feat: criação superuserpadrão

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -4,14 +4,22 @@
 set -e
 
 while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-  echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..."
+  echo "🟡 Espere for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..."
   sleep 2
 done
 
-echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
+echo "✅ Postgres Database Iniciou Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
 pip install -r requirements.txt && \
 python manage.py collectstatic --noinput
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
+
+echo "✅ Creating superuser 'root'..."
+export DJANGO_SUPERUSER_USERNAME=root
+export DJANGO_SUPERUSER_EMAIL=root@example.com 
+export DJANGO_SUPERUSER_PASSWORD=231212
+python manage.py createsuperuser --noinput || echo "Superuser 'root' already exists or an error occurred during creation."
+
+
 python manage.py runserver 0.0.0.0:8000 --verbosity 3
 
